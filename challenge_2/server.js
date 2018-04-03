52683cd814599d7ebcec33ac2bb5491db9409148
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-03 14:37:09 -0700

    Server up and returning index. All I had to do was move the `app.use` statement

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
-app.get('/', (req, res) =>  res.send('heya earth'));
-app.listen(3000, () => console.log(`Listening on 3000`));
+
+app.get('/', (req, res) => res.sendfile('client/index.html'));
+app.listen(3000, () => console.log('Listening on 3000'));
+
+app.use(express.static('client'));
 
 // app.post('user/add', (req, res) => {
 //   res.send('OK');
-// });
+// })
 
 // app.get(/^(.+)$/, (req, res) => {
 //   console.log('static file request: ' + req.params);
 //   res.sendfile(__dirname + req.params[0]);
-// });
+// })
