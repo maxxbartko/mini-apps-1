595fb1dd02982c8e6b288d1306ef0b54cb78f0ec
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-03 11:09:09 -0700

    Further setup. Server runs in circles and won't start.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+app.get('/', (req, res) =>  res.send('heya earth'));
+app.listen(3000, () => console.log(`Listening on 3000`));
 
-app.get('/', (req, res) => {
-  res.sendfile(client/index.html)
-});
+// app.post('user/add', (req, res) => {
+//   res.send('OK');
+// });
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+// app.get(/^(.+)$/, (req, res) => {
+//   console.log('static file request: ' + req.params);
+//   res.sendfile(__dirname + req.params[0]);
+// });
