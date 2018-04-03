afe5cdc13772c20d01a49f8dc941ca3ee7636ce7
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-03 10:55:03 -0700

    First steps

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var app = express();
 
+app.get('/', (req, res) => {
+  res.sendfile(client/index.html)
+});
 
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
