6a4a37cef4d6c069d28b55ceef032e3952b18904
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-03 19:55:00 -0700

    Post works again. Began app.js

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,18 +1,14 @@
 const express = require('express'),
-  app = express(),
-  $ = require('jQuery');
+  app = express();
+  
 
-
-app.get('/', (req, res) => res.sendFile('client/index.html'));
+app.get('/', (req, res) => res.sendfile('client/index.html'));
 app.listen(3000, () => console.log('Listening on 3000'));
 
 app.use(express.static('client'));
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.post('/', (req, res) => {
-  let $json = $('#json :submit').val();
-  console.log($json);
-  res.send($json);
+  res.send('ok');
 });
 
 // app.get(/^(.+)$/, (req, res) => {
