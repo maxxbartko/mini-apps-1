5d4bb6a7f47e6f73cda0be2feaceaefb045539ad
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-04 15:41:49 -0700

    Board and rows code fleshed out, not rendering due to an error

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,7 +1,44 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
+
+class App extends React.Component {
+//   initialize()
+  render() { return <Board/> }
+
+}
+
+const Board = props => {
+  const boardModel = [5, 4, 3, 2, 1, 0];
+  return (
+    <table>
+      { boardModel.map(row => (<tr row={row} key={`row${row}`}></tr>)) }
+    </table>
+  )
+}
+
+const Row = props => {
+  const rowModel = [0, 1, 2, 3, 4, 5, 6];
+  return (
+    rowModel.map(col => (<td key={`${this.props.row},${col}`} class="space"></td>))
+  )
+}
+
+Row.propTypes = {
+  row: React.PropTypes.number.isRequired
+}
+
+{/* const Piece = props => {
+
+}
+
+const UI = props => {
+
+} */}
+
+const ConnectFour = new App();
+
 ReactDOM.render(
-  <h1>ey earth how u doin!</h1>,
+  <ConnectFour/>,
   document.getElementById('app')
-);
+)
